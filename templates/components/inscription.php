49d2d955862f74c052478fd3be3b004afcb3dd17
Cory404 <coryletombe1@gmail.com>
2024-01-22 10:16:30 +0100

    les gars g fais du html

--- templates/components/inscription.php
+++ templates/components/inscription.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Connexion</title>
+    <title>Inscription</title>
 </head>
 <body>
-<h2>Connexion</h2>
+<h2>Inscription</h2>
 <p><?php echo $message; ?></p>
-<form method="post" action="connexion.php">
+<form method="post" action="inscription.php">
     Nom d'utilisateur: <input type="text" name="nomUtilisateur" required><br>
     Mot de passe: <input type="password" name="motDePasse" required><br>
-    <input type="submit" value="Se connecter">
+    <input type="submit" value="S'inscrire">
 </form>
-<p>Nouvel utilisateur? <a href='inscription.php'>Inscrivez-vous ici</a></p>
+<p>Déjà inscrit? <a href='connexion.php'>Connectez-vous ici</a></p>
 </body>
 </html>
